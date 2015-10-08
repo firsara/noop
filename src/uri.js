6ef126e2d86230e13ad6d65b66c20bf03aa43a8e
Fabian Irsara <info@fabianirsara.com>
2015-10-08 11:25:56 +0200

    add functions for parsing domain, protocol, etc

--- src/uri.js
+++ src/uri.js
@@ -5,8 +5,6 @@
  */
 define(function(){
 
-  // TODO: also parse TLD, path, etc.
-
   /**
    * @example
    *
@@ -19,17 +17,26 @@ define(function(){
     // store url that needs to be parsed
     this.url = url;
 
-    // assume url has no query string at all
-    var query = '';
+    // and parse protocol string properly
+    this.protocol = uri.extractProtocol(this.url);
 
-    // if it has a query string
-    if (this.url.indexOf('?') >= 0) {
-      // store it
-      query = this.url.substring(this.url.indexOf('?'));
-    }
+    // and parse domain string properly
+    this.domain = uri.extractDomain(this.url);
+
+    // and parse port string properly
+    this.port = uri.extractPort(this.url);
+
+    // and parse path string properly
+    this.path = uri.extractPath(this.url);
 
     // and parse query string properly
-    this.query = uri.parseQuery(query);
+    this.query = uri.parseQuery(this.url);
+
+    // and parse hash string properly
+    this.hash = uri.extractHash(this.url);
+
+    // and parse hostname properly
+    this.host = this.domain + (this.port.length > 0 ? ':' + this.port : '');
   }
 
   /**
@@ -41,7 +48,49 @@ define(function(){
    * @param {String} key to get from query string
    **/
   URI.prototype.getQuery = function(key){
+    if (! key) {
+      var parts = [];
+
+      for (var k in this.query) {
+        parts.push(k + '=' + this.query[k]);
+      }
+
+      var str = parts.join('&');
+      if (str.length > 0) str = '?' + str;
+
+      return str;
+    }
+
     return this.query[key];
+  };
+
+  /**
+   * joins parsed uri parts to generate full path
+   *
+   * @method join
+   * @memberof URI
+   * @instance
+   * @param {Array} options which parts should be included (includes all parts by default):
+   * * "protocol"
+   * * "domain"
+   * * "port"
+   * * "path"
+   * * "query"
+   * * "hash"
+   **/
+  URI.prototype.join = function(options){
+    if (! options) options = ['protocol', 'domain', 'port', 'path', 'query', 'hash'];
+
+    var str = '';
+
+    if (options.indexOf('protocol') !== -1) str += this.protocol;
+    if (options.indexOf('domain') !== -1) str += this.domain;
+    if (options.indexOf('port') !== -1) str += (this.port.length > 0 ? ':' + this.port : '');
+    if (options.indexOf('path') !== -1)  str += this.path;
+    if (options.indexOf('query') !== -1) str += this.getQuery();
+    if (options.indexOf('hash') !== -1) str += this.hash;
+
+    return str;
   };
 
   /**
@@ -68,21 +117,141 @@ define(function(){
   };
 
   /**
+   * extracts the domain of a specific url
+   *
+   * @exmaple
+   *
+   * uri.extractProtocol('http://www.youtube.com/?v=12345') // http://
+   * uri.extractProtocol('https://youtube.com/?v=12345') // https://
+   * uri.extractProtocol('file:///Users/username/path/file.png') // file://
+   * uri.extractProtocol('www.youtube.com') // ''
+   *
+   * @method extractProtocol
+   * @memberof uri
+   * @param {String} url that should be parsed
+   **/
+  uri.extractProtocol = function(url) {
+    var index = url.indexOf('://');
+    if (index === -1) return '';
+    return url.substring(0, index + 3);
+  };
+
+  /**
+   * extracts the domain of a specific url
+   *
+   * @exmaple
+   *
+   * uri.extractDomain('http://www.youtube.com/?v=12345') // www.youtube.com
+   * uri.extractDomain('https://youtube.com/?v=12345') // youtube.com
+   *
+   * @method extractDomain
+   * @memberof uri
+   * @param {String} url that should be parsed
+   **/
+  uri.extractDomain = function(url) {
+    var domain = null;
+
+    //find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+      domain = url.split('/')[2];
+    } else {
+      domain = url.split('/')[0];
+    }
+
+    //find & remove port number
+    return domain.split(':')[0];
+  };
+
+  /**
+   * extracts the domain of a specific url
+   *
+   * @exmaple
+   *
+   * uri.extractPort('http://www.youtube.com:1337/?v=12345') // 1337
+   * uri.extractPort('https://youtube.com/?v=12345') // ''
+   *
+   * @method extractPort
+   * @memberof uri
+   * @param {String} url that should be parsed
+   **/
+  uri.extractPort = function(url) {
+    url = url.replace(uri.extractProtocol(url), '');
+
+    var portIndex = url.indexOf(':');
+    var pathIndex = url.indexOf('/');
+    var queryIndex = url.indexOf('?');
+    var hashIndex = url.indexOf('#');
+
+    var subIndex = pathIndex;
+    if (queryIndex !== -1 && queryIndex < pathIndex) subIndex = queryIndex;
+    if (hashIndex !== -1 && hashIndex < pathIndex) subIndex = hashIndex;
+
+    if (portIndex === -1) return '';
+    if (subIndex !== -1 && subIndex < portIndex) return '';
+
+    if (subIndex === -1) return url.substring(portIndex + 1);
+    return url.substring(portIndex + 1, subIndex);
+  };
+
+  /**
+   * extracts the domain of a specific url
+   *
+   * @exmaple
+   *
+   * uri.extractPath('http://www.youtube.com:1337/?v=12345') // '/'
+   * uri.extractPath('http://youtube.com/users/1/') // '/users/1/'
+   * uri.extractPath('http://youtube.com') // ''
+   *
+   * @method extractPath
+   * @memberof uri
+   * @param {String} url that should be parsed
+   **/
+  uri.extractPath = function(url) {
+    url = url.replace(uri.extractProtocol(url), '');
+
+    var pathIndex = url.indexOf('/');
+    var queryIndex = url.indexOf('?');
+    var hashIndex = url.indexOf('#');
+
+    var subIndex = queryIndex;
+    if (hashIndex !== -1 && hashIndex < pathIndex) subIndex = hashIndex;
+
+    if (pathIndex === -1) return '';
+
+    if (subIndex === -1) return url.substring(pathIndex);
+    return url.substring(pathIndex, subIndex);
+  };
+
+  /**
    * parses a query string and returns a key-value object
    *
    * @exmaple
    *
    * uri.parseQuery('username=me&password=secret') // {username: 'me', password: 'secret'}
+   * uri.parseQuery('http://www.site.com/?username=me&password=secret') // {username: 'me', password: 'secret'}
    *
    * @method parseQuery
    * @memberof uri
-   * @param {String} query that should be parsed
+   * @param {String} url that should be parsed
    **/
-  uri.parseQuery = function(query){
+  uri.parseQuery = function(url){
+    // remove hash from url
+    url = url.replace(uri.extractHash(url), '');
+
+    // assume url has no query string at all
+    var query = '';
+
+    // if it has a query string
+    if (url.indexOf('?') >= 0) {
+      // store it
+      query = url.substring(url.indexOf('?'));
+    }
+
     var data = {};
     query = query.replace('?', '').split('&');
 
     for (var i = 0, _len = query.length; i < _len; i++) {
+      if (query[i].length === 0) continue;
       var keyValuePair = query[i].split('=');
       var key = keyValuePair[0];
       var value = keyValuePair[1];
@@ -93,6 +262,24 @@ define(function(){
     }
 
     return data;
+  };
+
+  /**
+   * extracts the domain of a specific url
+   *
+   * @exmaple
+   *
+   * uri.extractHash('http://www.youtube.com:1337/?v=12345') // ''
+   * uri.extractHash('http://youtube.com/users/1/#test') // '#test'
+   *
+   * @method extractHash
+   * @memberof uri
+   * @param {String} url that should be parsed
+   **/
+  uri.extractHash = function(url) {
+    var index = url.indexOf('#');
+    if (index === -1) return '';
+    return url.substring(index);
   };
 
   return uri;
