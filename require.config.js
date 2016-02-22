33d7df424856a7660e7453f33cdba8038b5c4d18
Fabian Irsara <info@fabianirsara.com>
2016-02-22 22:26:06 +0100

    upgrade dependencies

--- require.config.js
+++ require.config.js
@@ -15,9 +15,9 @@ require.config({
     }
   },
   paths: {
-    EaselJS: '../../bower_components/EaselJS/lib/easeljs-0.8.1.combined',
+    EaselJS: '../../bower_components/EaselJS/lib/easeljs-0.8.2.combined',
     PreloadJS: '../../bower_components/PreloadJS/lib/preloadjs-0.6.1.combined',
-    SoundJS: '../../bower_components/SoundJS/lib/soundjs-0.6.1.combined',
+    SoundJS: '../../bower_components/SoundJS/lib/soundjs-0.6.2.combined',
     gsap: '../../bower_components/gsap/src/uncompressed/TweenMax',
     handlebars: '../../bower_components/handlebars/handlebars',
     jquery: '../../bower_components/jquery/dist/jquery',
